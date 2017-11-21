b6a9157d24f70bee8fa6fd808f53a3d235af8dd1
Soha Jin <soha@lohu.info>
2017-11-21 13:15:28 +0800

    create poller.js

---
+++ README.md
@@ -0,0 +1,8 @@
+# Session Polling Helper
+
+## 会话轮询助手
+
+It is designed for WeChat public accounts' back-end to poll with user.
+
+----------
+
---
+++ index.js
@@ -0,0 +1,12 @@
+/*=============================
+Session Polling Helper
+-------------------------------
+Tianhai InfoTech
+	http://tianhai.info/
+Soha Jin
+	https://sohaj.in/
+=============================*/
+
+module.exports={
+	Poller: require('./lib/poller')
+}
